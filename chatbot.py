65a51dd25a24121a7d2b001b031ebb6459a93259
nishthavarlani-0104 <varlaninishtha@gmail.com>
2026-07-26 23:54:59 +0530

    Initial Chatbot

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,43 @@
-
+from langchain_groq import ChatGroq
+from langchain_core.messages import HumanMessage,AIMessage, SystemMessage
+import os
 from dotenv import load_dotenv
 load_dotenv()
 
-prompt= input("You : ")
+model=ChatGroq(model="openai/gpt-oss-120b") #chat model
+
+summarize_llm=ChatGroq(model="openai/gpt-oss-120b") #summary model
+
+print("="*60)
+print("                     Hello Welcome!          ")
+print("="*60)
+
+#Chat memory
+messages=[]
+
+print("\nType 'exit' to Exit")
+while True:
+    prompt= input("You : ")
+    messages.append(HumanMessage(content=prompt))
+    if prompt.lower()=="exit":
+        print("Thank you, I Hope it was a good conversation!")
+        break
+    response=model.invoke(messages)
+    messages.append(AIMessage(content=response.content))
+    print("Bot : ",response.content)
+
+    ## Summarize to save the memory
+    if len(messages)>=10:
+        summary_prompt="Summarize the following meassages into small paragraph: \n\n"
+        for msg in messages:
+            if isinstance(msg,HumanMessage):
+                summary_prompt+=f"User: {msg.content}\n"
+            else:
+                summary_prompt+=f"Bot: {msg.content}\n"   
+
+        summarize_response=summarize_llm.invoke(summary_prompt)
+        messages.clear()
+        messages.append(SystemMessage(content=summarize_response.content)) #Conversation History
+
+        print(messages)
 
